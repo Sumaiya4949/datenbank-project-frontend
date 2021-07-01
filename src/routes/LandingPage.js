cbee3dcfad923479c9d9455e44385d751fb97446
sinthy <sumaiya15-4949@diu.edu.bd>
2021-07-02 00:03:43 +0200

    Add css modules

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -1,19 +1,22 @@
 import logo from "../assets/icons/logo.svg"
-import "../styles/LandingPage.css"
+import { Button } from "antd"
+import styles from "../styles/LandingPage.module.css"
 
 export default function LandingPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.LandingPage}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
 
         <h2>Grading System Management</h2>
 
-        <p className="App-desc">
+        <p className={styles.description}>
           A full stack web application for digital management of students'
           grades and subjects in different classes in a school, along with
           additional management functionalities of admins and teachers.
         </p>
+
+        <div className={styles.loginPanel}></div>
       </header>
     </div>
   )
