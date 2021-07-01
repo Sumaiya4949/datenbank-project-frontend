a94ce7d64eaa99946df48389ef4cb08cc62ad042
sinthy <sumaiya15-4949@diu.edu.bd>
2021-07-02 01:12:13 +0200

    Use axios for auth login request

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,25 @@
+import axios from "axios"
 import { useCallback, useState } from "react"
+import { API_SERVER } from "../constants"
 
 export default function useAuth() {
   const [isRequesting, setRequesting] = useState(false)
   const [loggedInUser, setLoggedInUser] = useState(null)
 
-  const login = useCallback((username, password) => {
+  const login = useCallback(async (username, password) => {
     setRequesting(true)
-    throw new Error("meow")
+
+    try {
+      const response = await axios.post(`${API_SERVER}/auth/login`, {
+        username,
+        password,
+      })
+
+      setRequesting(false)
+    } catch (error) {
+      setRequesting(false)
+      throw error
+    }
   }, [])
 
   return { loggedInUser, isRequesting, login }
