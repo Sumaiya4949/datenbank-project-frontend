a4e8c12abf2e8dc844c20c75ef804f4a18da1f7e
sinthy <sumaiya15-4949@diu.edu.bd>
2021-07-02 19:45:25 +0200

    Update styles

--- src/components/RouteBreadcrumbs.js
+++ src/components/RouteBreadcrumbs.js
@@ -1,13 +1,19 @@
 import { Breadcrumb } from "antd"
+import { useMemo } from "react"
 import { useLocation } from "react-router-dom"
 
 export default function RouteBreadcrumbs() {
   const { pathname } = useLocation()
 
+  const pathNames = useMemo(() => {
+    const names = pathname.split("/")
+    names.unshift("home")
+    return names
+  }, [pathname])
+
   return (
     <Breadcrumb style={{ margin: "16px 0" }}>
-      {pathname.split("/").map((_name) => {
-        const name = _name === "" ? "home" : _name
+      {pathNames.map((name) => {
         return (
           <Breadcrumb.Item key={name} style={{ textTransform: "capitalize" }}>
             {name}
