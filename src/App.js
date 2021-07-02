6a72ddc2eff1aeac99644ca9d165c42754918fa9
sinthy <sumaiya15-4949@diu.edu.bd>
2021-07-02 14:02:37 +0200

    Add layout from ant

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import LandingPage from "./routes/LandingPage"
-import AppHeader from "./components/AppHeader"
 import useAuth from "./hooks/useAuth"
 import { AuthContext } from "./contexts"
 import { Route, Switch } from "react-router-dom"
 import HomePage from "./routes/HomePage"
 import InvalidRoute from "./routes/InvalidRoute"
+import { Layout, Menu, Breadcrumb } from "antd"
+import { Typography } from "antd"
+
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons"
+
+const { SubMenu } = Menu
+const { Header, Content, Sider } = Layout
 
 function App() {
   const auth = useAuth()
-
-  console.log(auth)
 
   if (!auth.loggedInUser)
     return (
@@ -20,13 +28,57 @@ function App() {
 
   return (
     <AuthContext.Provider value={auth}>
-      <AppHeader />
-      <main>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route component={InvalidRoute} />
-        </Switch>
-      </main>
+      <Layout className="app-layout">
+        <Header className="app-header">
+          <b className="app-title">Grading System Management</b>
+
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+
+        <Layout>
+          <Sider width={250}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              style={{ height: "100%", borderRight: 0 }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                <Menu.Item key="1">option1</Menu.Item>
+                <Menu.Item key="2">option2</Menu.Item>
+                <Menu.Item key="3">option3</Menu.Item>
+                <Menu.Item key="4">option4</Menu.Item>
+              </SubMenu>
+
+              <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                <Menu.Item key="5">option5</Menu.Item>
+                <Menu.Item key="6">option6</Menu.Item>
+                <Menu.Item key="7">option7</Menu.Item>
+                <Menu.Item key="8">option8</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+
+          <Layout style={{ padding: "0 24px 24px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+
+            <Content className="app-content-container">
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route component={InvalidRoute} />
+              </Switch>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     </AuthContext.Provider>
   )
 }
